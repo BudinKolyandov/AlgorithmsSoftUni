e92f7c35ad11ce9593151c0d48376efc6705391a
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-08-15 10:19:36 +0300

    Completed problems 1 and 2 from Recursion homework.

---
+++ RecursionHomework/NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NestedLoopsToRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+
+            Recursion(vector, 0);
+        }
+
+        private static void Recursion(int[] vector, int index)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+            }
+            else
+            {
+                for (int i = 1; i <= vector.Length; i++)
+                {
+                    vector[index] = i;
+                    Recursion(vector, index + 1);
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ RecursionHomework/ReverseArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            ReverseArray(array, 0, array.Length - 1);
+            Console.WriteLine(string.Join(" ", array));
+        }
+
+        public static void ReverseArray(int[] array, int lower, int upper)
+        {
+            for (int i = lower, j = upper; i < j; i++, j--)
+            {
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+    }
+}
