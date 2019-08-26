2be3e479cd4efd3311d393d7a4ab9bf104fccf37
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-08-26 08:48:22 +0300

    Implemented the Quicksort algorithm.

---
+++ SortingAndSearching/Quicksort/Quicksort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Quicksort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+
+            Quick.Sort<int>(arr);
+
+            Console.WriteLine(string.Join(' ', arr));
+        }
+    }
+}
