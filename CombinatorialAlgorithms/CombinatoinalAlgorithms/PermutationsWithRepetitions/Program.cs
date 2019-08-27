d4618159683b653e34ce00ae825997c0afc4ff22
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-08-27 14:33:14 +0300

    Completed permutations lab exercises.

---
+++ CombinatorialAlgorithms/CombinatoinalAlgorithms/PermutationsWithRepetitions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PermutationsWithRepetitions
+{
+    class Program
+    {
+        static int[] arr;
+
+        static void Permute(int[] arr, int start, int end)
+        {
+            Console.WriteLine(string.Join(' ', arr));
+            for (int left = end; left >= start; left--)
+            {
+                for (int right = left + 1 ; right <= end; right++)
+                {
+                    if (arr[left] != arr[right] && arr[left] < arr.Length - 1  && arr[right] < arr.Length - 1)
+                    {
+                        Swap(arr[left], arr[right]);
+                        Permute(arr, left + 1, end);
+                    }
+                    var firstElement = arr[left];
+                    for (int i = left; i < end; i++)
+                    {
+                        arr[i] = arr[i + 1];
+                    }
+                    arr[end] = firstElement;
+                }
+            }
+        }
+
+        static void Swap(int first, int second)
+        {
+            var temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+
+
+        static void Main(string[] args)
+        {
+            arr = new[] { 1, 3, 5, 2, 3, };
+            Array.Sort(arr);
+            Permute(arr, 0, arr.Length - 1);
+        }
+    }
+}
---
+++ CombinatorialAlgorithms/CombinatoinalAlgorithms/PermutationsWithoutRepetitions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PermutationsWithoutRepetitions
+{
+    class Program
+    {
+        static int[] elements;
+
+        static void Permute(int index)//index is current cell to fill
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(' ', elements));
+            }
+            else
+            {
+                Permute(index + 1);
+                for (int i = index + 1; i < elements.Length; i++)
+                {
+                    Swap(index, i);
+                    Permute(index + 1);
+                    Swap(index, i);
+                }
+            }
+        }
+
+        static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
+        }
+
+
+        static void Main(string[] args)
+        {
+            elements = new[] { 1, 2, 3, 4 };         
+            Permute(0);
+        }
+    }
+}
